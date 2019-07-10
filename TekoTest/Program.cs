801f8e0878991ba3222cccea2a836727f79eb650
zloodeyka <khusnutdinovazi@gmail.com>
2019-07-10 10:54:30 +0500

    Initial commit

---
+++ TekoTest/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace TekoTest
+{
+
+    class Program
+    {
+        public static string GetVacationsIntersectionReport(List<Vacation> myVacations, List<Vacation> depVacations)
+        {
+            StringBuilder result = new StringBuilder();
+
+            foreach (var depVacation in depVacations)
+            {
+                result.Append($"ID {depVacation.UserId} {depVacation.GetDates()}");
+
+                string intersectionDates = string.Empty;
+
+                foreach (var myVacation in myVacations)
+                {
+                    if (depVacation.HasIntersection(myVacation))
+                    {
+                        if (!string.IsNullOrEmpty(intersectionDates))
+                        {
+                            intersectionDates = $"{intersectionDates}, ";
+                        }
+
+                        intersectionDates = $"{intersectionDates}{myVacation.GetDates()}";
+                    }
+                }
+
+                if (!string.IsNullOrEmpty(intersectionDates))
+                {
+                    result.Append($" пересекается с {intersectionDates}");
+                }
+
+                result.Append("\r\n");
+            }
+
+            return result.ToString();
+        }
+
+        static void Main(string[] args)
+        {
+            List<Vacation> myVacations = new List<Vacation>();
+
+            int vacationsYear = 2020;
+            int vacationDuration = 2 * 7 - 1;
+
+            myVacations.Add(new Vacation(1, vacationsYear, vacationDuration));
+
+            myVacations.Add(myVacations.Last().GetNonintersectVacation(vacationsYear, vacationDuration)); 
+
+            List<Vacation> depVacations = new List<Vacation>();
+
+            for (int i = 0; i < 500; i++)
+            {
+                depVacations.Add(new Vacation(i + 2, vacationsYear, vacationDuration));
+                depVacations.Add(depVacations.Last().GetNonintersectVacation(vacationsYear, vacationDuration));
+            }
+
+            File.WriteAllText("result.txt", GetVacationsIntersectionReport(myVacations, depVacations));
+
+        }
+    }
+}
---
+++ TekoTest/Vacation.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace TekoTest
+{
+    class Vacation
+    {
+        public int UserId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        private static Random _random;
+
+        static Vacation()
+        {
+            _random = new Random();
+        }
+
+        /// <summary>
+        /// Init random vacation period
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="year"></param>
+        /// <param name="duration"></param>
+        public Vacation(int userId, int year, int duration)
+        {
+            UserId = userId;
+
+            int yearDays = 365;
+            int validVacationStart = yearDays - duration;
+            StartDate = (new DateTime(year, 01, 01)).AddDays(_random.Next(validVacationStart));
+            EndDate = StartDate.AddDays(duration);
+        }
+
+        public bool HasIntersection(Vacation vacation)
+        {
+            return StartDate <= vacation.StartDate && vacation.StartDate <= EndDate
+                   || StartDate <= vacation.EndDate && vacation.EndDate <= EndDate;
+        }
+
+        /// <summary>
+        /// Get nonintersect random vacation
+        /// </summary>
+        /// <param name="year"></param>
+        /// <param name="duration"></param>
+        /// <returns></returns>
+        public Vacation GetNonintersectVacation(int year, int duration)
+        {
+            Vacation vacation = new Vacation(UserId, year, duration);
+
+            while (this.HasIntersection(vacation))
+            {
+                vacation = new Vacation(UserId, year, duration);
+            }
+
+            return vacation;
+        }
+
+        public string GetDates()
+        {
+            return $"{StartDate.ToShortDateString()}-{EndDate.ToShortDateString()}";
+        }
+    }
+}
